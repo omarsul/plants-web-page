4a1262863f4435e7cd65bf5a46422f54e59dbe79
omar alsultan <omarsu.dev@gmail.com>
2022-09-26 01:02:05 +0300

    webPage added

---
+++ easy.js
@@ -0,0 +1,12 @@
+
+function changePic1() {
+    document.getElementById("myImg").src ="assets/succulents-2.jpg"; 
+  }
+function changePic2(){
+    document.getElementById("myImg").src ="assets/succulents-1.jpg"; 
+  }
+
+function accept() {
+    const element = document.getElementById("cookie");
+    element.remove(); 
+}
